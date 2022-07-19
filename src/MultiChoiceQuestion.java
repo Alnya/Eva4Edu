014e034490f7e158152ceb0fe820ed97b61c8ae5
Alnya <69311410+Alnya@users.noreply.github.com>
2022-07-19 17:17:32 +0900

    add: MultiChoiceQuestion.java

---
+++ src/MultiChoiceQuestion.java
@@ -0,0 +1,8 @@
+public class MultiChoiceQuestion extends Question {
+    public MultiChoiceQuestion() {
+    }
+
+    public MultiChoiceQuestion(String name) {
+        super.setName(name);
+    }
+}
--- src/Question.java
+++ src/Question.java
@@ -8,4 +8,20 @@ public class Question {
     public Question(String name) {
         this.name = name;
     }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getLevel() {
+        return this.level;
+    }
 }
