9ec20331536967b48af88a08ac0332bc63737203
Alnya <69311410+Alnya@users.noreply.github.com>
2022-07-21 11:17:35 +0900

    update: MultiChoiceQuestion.java

--- src/MultiChoiceQuestion.java
+++ src/MultiChoiceQuestion.java
@@ -1,8 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class MultiChoiceQuestion extends Question {
+    private List<String> choices = new ArrayList<>();
+
     public MultiChoiceQuestion() {
     }
 
     public MultiChoiceQuestion(String name) {
         super.setName(name);
+    }
+
+    public void setChoices(List<String> choices) {
+        this.choices = choices;
+    }
+
+    public List<String> getChoices() {
+        return this.choices;
     }
 }
